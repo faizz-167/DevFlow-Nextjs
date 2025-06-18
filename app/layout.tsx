693caa7a6a4ecf7fe949044a0f6c3b4c693ee1ad
Faiz <220701167@rajalakshmi.edu.in>
2025-06-18 18:29:49 +0530

    added assets , setuping eslint and tailwing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,38 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = Inter({
+    variable: "--font-inter",
+    subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spaceGrotesk = SpaceGrotesk({
+    variable: "--font-space-grotesk",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Devflow",
-  description: "Devflow is a platform for developers to share and discover code snippets, tutorials, and resources.",
+    title: "Devflow",
+    description:
+        "Devflow is a platform for developers to share and discover code snippets, tutorials, and resources.",
+    icons: {
+        icon: "/images/site-logo.svg",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Welcome to Devflow</h1>
+            <h1 className="text-3xl font-bold font-space-grotesk">
+                Welcome to Devflow
+            </h1>
+        </div>
+    );
+};
 
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold">Welcome to Devflow</h1>
-  );
-}
+export default Home;
