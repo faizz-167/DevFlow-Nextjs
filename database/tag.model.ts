d2b411ca20f22d45e2674a914b850f4e717781c1
Faiz <220701167@rajalakshmi.edu.in>
2025-07-06 23:23:03 +0530

    implement required all database models

---
+++ database/tag.model.ts
@@ -0,0 +1,23 @@
+import { model, models, Schema } from "mongoose";
+
+export interface ITag {
+    name: string;
+    questions: number;
+}
+
+const TagSchema = new Schema<ITag>(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        questions: {
+            type: Number,
+            default: 0,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Question = models.Tag || model<ITag>("Tag", TagSchema);
