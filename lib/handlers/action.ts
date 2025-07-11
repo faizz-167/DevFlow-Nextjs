3e11680a3303a7d3d10da6eb1644e0e370bf9654
Faiz <220701167@rajalakshmi.edu.in>
2025-07-11 10:26:01 +0530

    implement action handler

---
+++ lib/handlers/action.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { ZodError, ZodSchema } from "zod";
+import { UnauthorizedError, ValidationError } from "../http=errors";
+import { Session } from "next-auth";
+import { auth } from "@/auth";
+import dbConnect from "../mongoose";
+
+type ActionOptions<T> = {
+    params?: T;
+    schema?: ZodSchema<T>;
+    authorize?: boolean;
+};
+
+async function action<T>({ params, schema, authorize }: ActionOptions<T>) {
+    if (schema && params) {
+        try {
+            schema.parse(params);
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return new ValidationError(
+                    error.flatten().fieldErrors as Record<string, string[]>
+                );
+            } else {
+                return new Error("schema validation failed");
+            }
+        }
+    }
+
+    let session: Session | null = null;
+    if (authorize) {
+        session = await auth();
+        if (!session) {
+            return new UnauthorizedError();
+        }
+    }
+
+    await dbConnect();
+
+    return { params, session };
+}
+
+// 1. checking if the schema and params are provided and are validated
+// 2. checking whether the user is authorized
+// 3. connecting to the database
+// 4. returning the validated params and session
+export default action;
