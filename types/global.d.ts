1c7ec52fe7b5245649dff4f520d600921729c68c
Faiz <220701167@rajalakshmi.edu.in>
2025-07-27 17:38:46 +0530

    implement answer votes

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,36 +1,36 @@
 interface Tag {
-  _id: string;
-  name: string;
+    _id: string;
+    name: string;
 }
 
 interface Author {
-  _id: string;
-  name: string;
-  image: string;
+    _id: string;
+    name: string;
+    image: string;
 }
 
 interface Question {
-  _id: string;
-  title: string;
-  content: string;
-  tags: Tag[];
-  author: Author;
-  createdAt: Date;
-  upvotes: number;
-  downvotes: number;
-  answers: number;
-  views: number;
-  createdAt: Date;
+    _id: string;
+    title: string;
+    content: string;
+    tags: Tag[];
+    author: Author;
+    createdAt: Date;
+    upvotes: number;
+    downvotes: number;
+    answers: number;
+    views: number;
+    createdAt: Date;
 }
 
 type ActionResponse<T = null> = {
-  success: boolean;
-  data?: T;
-  error?: {
-    message: string;
-    details?: Record<string, string[]>;
-  };
-  status?: number;
+    success: boolean;
+    data?: T;
+    error?: {
+        message: string;
+        details?: Record<string, string[]>;
+    };
+    status?: number;
 };
 
 type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
@@ -40,21 +40,23 @@ type APIErrorResponse = NextResponse<ErrorResponse>;
 type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
 
 interface RouteParams {
-  params: Promise<Record<string, string>>;
-  searchParams: Promise<Record<string, string>>;
+    params: Promise<Record<string, string>>;
+    searchParams: Promise<Record<string, string>>;
 }
 
 interface PaginatedSearchParams {
-  page?: number;
-  pageSize?: number;
-  query?: string;
-  filter?: string;
-  sort?: string;
+    page?: number;
+    pageSize?: number;
+    query?: string;
+    filter?: string;
+    sort?: string;
 }
 
 interface Answer {
-  _id: string;
-  author: Author;
-  content: string;
-  createdAt: Date;
+    _id: string;
+    author: Author;
+    content: string;
+    upvotes: number;
+    downvotes: number;
+    createdAt: Date;
 }
